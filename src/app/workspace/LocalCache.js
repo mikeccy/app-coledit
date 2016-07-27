6d823698e2232a60621e7224899c6f0056fff0f8
Chongyu Chen <mikeccy@gmail.com>
2016-07-27 01:17:37 -0700

    Clean up code base.

---
+++ src/app/workspace/LocalCache.js
@@ -0,0 +1,5 @@
+LocalCache.js
+user memory cache
+if data is not there then load from remote, render on canvas save to memory
+
+group fetch functionality => when all the query is cached then render/callback
