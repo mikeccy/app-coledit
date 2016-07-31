ce318d9ff846250c9161de0b623643760a250847
Chongyu Chen <mikeccy@gmail.com>
2016-07-30 22:53:21 -0700

    Add toggle play pause with space.

--- src/app/workspace/component/Playback.js
+++ src/app/workspace/component/Playback.js
@@ -1,7 +1,27 @@
 import Component from 'react-pure-render/component';
 import React from 'react';
+import KeyHandler from 'react-key-handler';
 
 class Playback extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this._togglePlay = this._togglePlay.bind(this);
+  }
+
+  _togglePlay(event) {
+    event.preventDefault();
+    if (this.props.playing) {
+      if (this.props.onPausePlaying) {
+        this.props.onPausePlaying();
+      }
+    } else {
+      if (this.props.onStartPlaying) {
+        this.props.onStartPlaying();
+      }
+    }
+  }
 
   render() {
     const {
@@ -13,6 +33,7 @@ class Playback extends Component {
 
     return (
       <div>
+        <KeyHandler keyEventName={'keydown'} keyValue=" " onKeyHandle={this._togglePlay} />
         <button style={{ marginRight : 10, }} className="playStop button button-primary" onClick={onStopPlaying}>停止</button>
         <button style={{ marginRight : 10, }} className="playStart button button-primary" onClick={onStartPlaying} disabled={playing}>开始</button>
         <button style={{ marginRight : 10, }} className="playPause button button-primary" onClick={onPausePlaying} disabled={!playing}>暂停</button>
